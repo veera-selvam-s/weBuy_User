4412aefd6056a95f8526bd9651625ae0033c9a58
web-veera <johndoe@example.com>
2021-12-15 21:07:39 +0530

    19_cmplt_fetchProducts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './containers/HomePage';
+import ProductListPage from './containers/ProductListPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={HomePage} />
+          <Route path="/:slug" component={ProductListPage} />
         </Switch>
       </Router>
     </div>
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Header from "../../components/Header";
-import MenuHeader from "../../components/MenuHeader";
-//import Layout from '../../components/Layout'
+import Layout from '../../components/Layout'
 
 /**
 * @author
@@ -10,10 +8,9 @@ import MenuHeader from "../../components/MenuHeader";
 
 const HomePage = (props) => {
   return (
-    <div>
-      <Header />
-      <MenuHeader />
-    </div>
+    <Layout>
+      Home Page
+    </Layout>
   );
 
  }
