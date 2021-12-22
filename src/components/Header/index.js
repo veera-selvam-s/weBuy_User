8a2b6de55b3218835bf1ea294aeb0c217cbab204
web-veera <veeraselvam965@gmail.com>
2021-12-22 17:17:59 +0530

    31_productyDetailo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import logo from '../../logo.png'
-import goldenStar from '../../images/logo/golden-star.png';
 import { IoIosArrowDown, IoIosCart, IoIosSearch } from 'react-icons/io';
-import { 
+import {
   Modal,
   MaterialInput,
   MaterialButton,
   DropdownMenu
 } from '../MaterialUI';
-import { useDispatch } from 'react-redux';
-import { login } from '../../actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, signout } from '../../actions';
+import {NavLink} from 'react-router-dom';
 
 /**
 * @author
@@ -18,20 +18,78 @@ import { login } from '../../actions';
 **/
 
 const Header = (props) => {
-
   const [loginModal, setLoginModal] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const auth = useSelector(state => state.auth);
   const dispatch = useDispatch();
 
   const userLogin = () => {
     dispatch(login({ email, password }));
   }
-  
+  const logout = () => {
+    dispatch(signout());
+  }
+
+  useEffect(() => {
+    if (auth.authenticate) {
+      setLoginModal(false)
+    }
+  }, [auth.authenticate]);
+  const renderLoggedInMenu = () => {
+    return (
+      <DropdownMenu
+        menu={
+          <a className="fullName">
+            {auth.user.firstName}
+          </a>
+        }
+        menus={[
+          { label: 'My Profile', href: '', icon: null },
+          { label: 'SuperCoin Zone', href: '', icon: null },
+          { label: 'Flipkart Plus Zone', href: '', icon: null },
+          { label: 'Orders', href: '', icon: null },
+          { label: 'Wishlist', href: '', icon: null },
+          { label: 'My Chats', href: '', icon: null },
+          { label: 'Coupons', href: '', icon: null },
+          { label: 'Rewards', href: '', icon: null },
+          { label: 'Notifications', href: '', icon: null },
+          { label: 'Gift Cards', href: '', icon: null },
+          { label: 'Logout', href: '', icon: null, onClick: logout }
+        ]}
+      />
+    );
+  }
+
+  const renderNonLoggedInMenu = () => {
+    return (
+      <DropdownMenu
+        menu={
+          <a className="loginButton" onClick={() => setLoginModal(true)}>
+            Login
+          </a>
+        }
+        menus={[
+          { label: 'My Profile', href: '', icon: null },
+          { label: 'Flipkart Plus Zone', href: '', icon: null },
+          { label: 'Orders', href: '', icon: null },
+          { label: 'Wishlist', href: '', icon: null },
+          { label: 'Rewards', href: '', icon: null },
+          { label: 'Gift Cards', href: '', icon: null },
+        ]}
+        firstMenu={
+          <div className="firstmenu">
+            <span>New Customer?</span>
+            <a style={{ color: '#2874f0' }}>Sign Up</a>
+          </div>
+        }
+      />
+    );
+  }
 
   return (
     <div className="header">
-      <Modal 
+      <Modal
         visible={loginModal}
         onClose={() => setLoginModal(false)}
       >
@@ -42,46 +100,46 @@ const Header = (props) => {
               <p>Get access to your Orders, Wishlist and Recommendations</p>
             </div>
             <div className="rightspace">
-          
 
-                <MaterialInput 
-                  type="text"
-                  label="Enter Email/Enter Mobile Number"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
 
-                <MaterialInput 
-                  type="password"
-                  label="Enter Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  // rightElement={<a href="#">Forgot?</a>}
-                />
-                
-                <MaterialButton 
-                  title="Login"
-                  bgColor="#F7931E"
-                  textColor="#ffffff"
-                  style={{
-                    margin: '40px 0 20px 0'
-                  }}
-                  onClick={userLogin}
-                />
+              <MaterialInput
+                type="text"
+                label="Enter Email/Enter Mobile Number"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-                <p>OR</p>
+              <MaterialInput
+                type="password"
+                label="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              // rightElement={<a href="#">Forgot?</a>}
+              />
 
-                <MaterialButton 
-                  title="Request OTP"
-                  bgColor="#ffffff"
-                  textColor="#2874f0"
-                  style={{
-                    margin: '20px 0'
-                  }}
-                />
-                
+              <MaterialButton
+                title="Login"
+                bgColor="#F7931E"
+                textColor="#ffffff"
+                style={{
+                  margin: '40px 0 20px 0'
+                }}
+                onClick={userLogin}
+              />
 
-              
+              <p>OR</p>
+
+              <MaterialButton
+                title="Request OTP"
+                bgColor="#ffffff"
+                textColor="#2874f0"
+                style={{
+                  margin: '20px 0'
+                }}
+              />
+
+
+
 
             </div>
           </div>
@@ -100,9 +158,11 @@ const Header = (props) => {
           </a>
         </div> */}
         <div className="logo">
-          <img src={logo} className="logoimage" alt="" />
+          <NavLink exact to={`/`}>
+            <img src={logo} className="logoimage" alt="" />
+          </NavLink>
         </div>
-        
+
         {/* logo ends here */}
 
         {/* search component */}
@@ -126,27 +186,10 @@ const Header = (props) => {
 
         {/* right side menu */}
         <div className="rightMenu">
-          <DropdownMenu
-            menu={
-              <a className="loginButton" onClick={() => setLoginModal(true)}>
-                Login
-              </a>
-            }
-            menus={[
-              { label: 'My Profile', href: '', icon: null },
-              { label: 'WeBuy Zone', href: '', icon: null },
-              { label: 'Orders', href: '', icon: null },
-              { label: 'Wishlist', href: '', icon: null },
-              { label: 'Rewards', href: '', icon: null },
-              { label: 'Gift Cards', href: '', icon: null },
-            ]}
-            firstMenu={
-              <div className="firstmenu">
-                <span>New Customer?</span>
-                <a style={{ color: '#2874f0' }}>Sign Up</a>
-              </div>
-            }
-          />
+          {
+            auth.authenticate ?
+              renderLoggedInMenu() : renderNonLoggedInMenu()
+          }
           <DropdownMenu
             menu={
               <a className="more">
