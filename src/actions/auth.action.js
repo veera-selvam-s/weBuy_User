cde0638f89d72b842c7a6e960e572e78698c0b1c
web-veera <veeraselvam965@gmail.com>
2021-12-22 19:03:15 +0530

    32_pdpageflex

--- src/actions/auth.action.js
+++ src/actions/auth.action.js
@@ -55,20 +55,17 @@ export const isUserLoggedIn = () => {
 
 export const signout = () => {
     return async dispatch => {
-
         dispatch({ type: authConstants.LOGOUT_REQUEST });
-        const res = await axios.post(`/admin/signout`);
-
-        if(res.status === 200){
-            localStorage.clear();
-            dispatch({ type: authConstants.LOGOUT_SUCCESS });
-        }else{
-            dispatch({
-                type: authConstants.LOGOUT_FAILURE,
-                payload: { error: res.data.error }
-            });
-        }
-
-        
+        localStorage.clear();
+        dispatch({ type: authConstants.LOGOUT_SUCCESS });
+        //const res = await axios.post(`/admin/signout`);
+        // if(res.status === 200){
+            
+        // }else{
+        //     dispatch({
+        //         type: authConstants.LOGOUT_FAILURE,
+        //         payload: { error: res.data.error }
+        //     });
+        // }  
     }
 }
