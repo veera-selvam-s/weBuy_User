41257cc3ec6841665b56989bf91d4c062b31cde0
web-veera <veeraselvam965@gmail.com>
2022-07-03 10:21:41 +0530

    urlupdate

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,9 +1,9 @@
 // export const api = `https://backendveera.run-ap-south1.goorm.io/api`;//api address
 // http://localhost:2000
-export const api =`https://webuybackend.run-ap-south1.goorm.io/api`;
+export const api =`http://localhost:2000/api`;
 export const generatePublicUrl = (fileName) => {
-  return `https://webuybackend.run-ap-south1.goorm.io/public/${fileName}`;
+  return `http://localhost:2000/public/${fileName}`;
 };
 export const pageUrl=(fileName)=>{
-  return `https://webuybackend.run-ap-south1.goorm.io${fileName}`;
+  return `http://localhost:2000${fileName}`;
 }
